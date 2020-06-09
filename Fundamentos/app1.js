2d0d724c0a385e0faf9660a8ae458fb3f6add472
unknown <alejandrojzeballos@gmail.com>
2020-06-09 02:56:43 -0400

    verifico

---
+++ Fundamentos/app1.js
@@ -0,0 +1,7 @@
+/* es un lenguaje de programacion */
+let nombre = prompt('cual es tu nombre');
+let edad = prompt('cual es tu edad?');
+/* crea un formulario */
+/* declaracion de variables */
+ document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
+ /* las comillas invertidas te perimten juntar la sintaxis de cadena con comandos */
