713404d89b4967a5b6d293a64bd14c11f1b4577f
unknown <alejandrojzeballos@gmail.com>
2020-06-09 17:56:41 -0400

    consola de js 1 parte

---
+++ Fundamentos/consola.js
@@ -0,0 +1,25 @@
+/* consola, Se puede identificar errores, codificar */
+/* prompt('Cual es tu nombre');
+"20"
+alert('Erros')
+undefined
+confirm('Deseas borrar ese elemento?');
+confirm('Deseas borrar ese elemento?');
+confirm('Deseas borrar ese elemento?');
+confirm('Deseas borrar ese elemento?');
+false
+true
+funciones en consola lo que se puede hacer tambien se puede realizar operaciones
+document devuelve el global de la pagina(toda la estructura) 
+se pude seleccionar todo tipo de etiquetas ejemplo:
+-------------------------------------------
+document.querySelector('h1');
+<h1>​Aprendiendo JavaScript Moderno​</h1>​
+*/
+/* //enviando valores a la consola */
+/* console.log("Enviando a la consola"); */
+/* permite ver si los valores son correctos */
+console.log(2+2);
+console.log('hasta aqui todo bien');
+console.log(true);
+console.log('ID:'+20);
