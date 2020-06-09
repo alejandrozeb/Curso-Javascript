0031a8b9db8729faf07239a56be37d200821736c
unknown <alejandrojzeballos@gmail.com>
2020-06-09 18:37:26 -0400

    variables con var en js

---
+++ Fundamentos/variables.js
@@ -0,0 +1,37 @@
+/* 3 formas de crear variables
+var 
+let
+const 
+*/
+var nombre = 'Juan';
+var nombre ='Jhon';
+/* no da error */
+var nombre;
+/* sin contenido */
+var carrito;
+carrito='libro';
+/* declarando mas de una variable */
+var carrito='libro', carrito1='libro 2';
+console.log(carrito);
+console.log(carrito1);
+/* cosas importantes cuando nombras variables */
+/* var 99dias;
+error los numeros al inicio no estan permitidos */
+
+var dias99=99;
+console.log(dias99);
+/* pero si una cadena y numero 
+crear variables relacionadas,significatias son recomendadas*/
+
+/* formas de nombrar variables */
+var primerNombre='Juan'; //camelcase
+var primer_nombre='Juan'; //underscore
+var PrimerNombre='Juan'; // pascal case
+var primernombre='Juan'; // no recomendado
+
+
+
+
+
+
+console.log();
