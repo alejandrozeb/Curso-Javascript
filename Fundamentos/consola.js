8d7e52900a95ec75463d529f55c87a74bf413e61
unknown <alejandrojzeballos@gmail.com>
2020-06-09 18:18:34 -0400

    consola de js parte 2

--- Fundamentos/consola.js
+++ Fundamentos/consola.js
@@ -23,3 +23,21 @@ console.log(2+2);
 console.log('hasta aqui todo bien');
 console.log(true);
 console.log('ID:'+20);
+/* El console.log devuelve un valor undefined la mayoria de las funciones retornan algun valor como el promt devuelve el valor ingresado */
+/* var hola='hola mundo';
+undefined
+console.log(hola);
+hola mundo
+console.log([1,2,3]);
+ (3) [1, 2, 3]
+console.table([1,2,3]); imprime como tabla
+console.error('algo salio mal'); 
+console.warn('algo salio mal');
+tambien para medir el tiempo de etiquetas entre el codigo*/
+console.time('hola');
+console.warn('hubo un error');
+console.warn('hubo un error');
+console.warn('hubo un error');
+console.warn('hubo un error');
+console.timeEnd('hola');
+ 
