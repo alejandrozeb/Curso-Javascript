fbda57addc9ec073ae260094772d004cf4a06e44
unknown <alejandrojzeballos@gmail.com>
2020-06-10 01:44:27 -0400

    numeros con js

---
+++ 1 - Fundamentos/6-numeros.js
@@ -0,0 +1,58 @@
+/* numeros en javascript */
+//instancas
+const numero1=30;
+const numero2=20;
+const numero3=20.20;
+const numero4=.1020;
+const numero5=-3;
+
+let resultado;
+//suma
+resultado= numero1 + numero2;
+//resta
+resultado=numero1 - numero2;
+//multiplicar
+resultado= numero1 *2;
+//divisiones
+resultado= numero1 / numero2;
+//modulo
+resultado=numero1%numero2;
+/* pi */
+resultado=Math.PI;
+/* redondea cell hacia arriba, floor hacia abajo*/
+resultado=Math.round(2.4);
+/* raiz cuadrada*/
+resultado=Math.sqrt(144);
+//absoluto
+resultado=Math.abs(numero5);
+//potencia
+resultado=Math.pow(8,3);
+//minino
+resultado=Math.min(3,6,7,5,8,9,);
+//maximo
+resultado=Math.max(3,6,5,7,8,9);
+//aleatorio
+resultado=Math.random();
+/* orden de operaciones es igual a las reglas de jerarquia de operadores pero tambien se pude hacer el uso de parentesis*/
+resultado=30/6*2
+/* ejemplo */
+resultado=(10+20+20+10+40)*.20;
+
+/* incremneto en consola 
+puntaje=10
+10
+puntaje
+10
+puntaje++
+10
+puntaje
+11 asigna y despues puedes imprimirlo
+------------------------
+puntaje=10
+10
+++puntaje
+11 asgina antes y lo imprime
+*/
+let puntaje;
+
+console.log(puntaje);
