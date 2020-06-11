45006f312a303623fc7b2bd20d43f707d036280d
unknown <alejandrojzeballos@gmail.com>
2020-06-11 01:47:42 -0400

    convertir numeros parte 2 en js

--- 1 - Fundamentos/9-convertir.js
+++ 1 - Fundamentos/9-convertir.js
@@ -12,3 +12,29 @@ console.log( Number(numero1)+numero2);
 console.log(parseInt(numero1)-numero2);
 /* cuando trata de convertir un valor no nuerico devuelve el valor NaN (not a number)*/
 console.log(Number(numero3));
+
+let dato;
+dato = Number("20");
+dato= Number('20.10931');
+dato= Number(true);
+/* el resultado es 1 por que se asocia con 1 = true y 0 = false */
+dato= Number(null);
+dato= Number(undefined);
+dato= Number('hola mundo');
+dato= Number([1,2,3,4,5]);
+
+//parseint,parsefloat
+
+dato= parseInt('100');
+dato= parseFloat('100');
+dato= parseFloat('100.2030');
+dato= parseInt('100');
+console.log(dato);
+/* tofixed solo aplica numero que ayuda a fijar una cantidad de digitos de un numero */
+dato=18929173.34324234;
+console.log(dato.toFixed(3));
+
+dato='189291173';
+console.log(parseInt(dato).toFixed(2));
+
+
