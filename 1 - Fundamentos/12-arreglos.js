a5406d8b5c97742403d517d583b399b93d64a65d
unknown <alejandrojzeballos@gmail.com>
2020-06-11 19:51:20 -0400

    arrays parte 2 en js

--- 1 - Fundamentos/12-arreglos.js
+++ 1 - Fundamentos/12-arreglos.js
@@ -24,7 +24,32 @@ console.log(meses[1]);
 /* para ingresar un nuevo elemento al array */
 meses[4]='mayo';
 console.log(meses);
+/* -----------------funciones en arrays-----------*/
 /* push agrega al final */
-
 meses.push('Junio');
 console.log(meses);
+
+/* encontrar un elemnto en el arreglo te devuelve la posicion en el que se encuentra */
+console.log(meses.indexOf('abril'));
+
+/* para agregar al incio, recorre los demas elementos*/
+
+meses.unshift('Mes 0');
+console.log(meses);
+
+/* eliminar un elemento de un arreglo */
+meses.pop();
+console.log(meses);
+/* elimina el primer elemento */
+meses.shift();
+console.log(meses);
+/* quitar un rango, para que solo se quite un elemneto el 1 es necesario al lado de la ,el segundo argumetno es la cantidad de elementos que se eleminaran desde la posicion del primer elemento */
+meses.splice(2,1);
+console.log(meses);
+/* revertir el array */
+meses.reverse();
+console.log(meses);
+/* para unir arreglos */
+let arreglo1=[1,2,3],
+    arreglo2=[9,8,7];
+    console.log(arreglo1.concat(arreglo2));
