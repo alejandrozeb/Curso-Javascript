693e2547cde891a2fe6fe41fdf3b53989305057b
unknown <alejandrojzeballos@gmail.com>
2020-06-12 02:54:14 -0400

    funciones parte 2 en js

--- 1 - Fundamentos/15-funciones.js
+++ 1 - Fundamentos/15-funciones.js
@@ -36,3 +36,47 @@ suma=sumar2(3,4);
 suma=sumar2(1,6);
 
 console.log(suma);
+
+/* parte 2 */
+/* cuando no ingresas los datsos o son erroneos en cadenas te devuelve undefined acompañado de el mensaje y en numeros nan, pero se pude definir valores por defecto */
+
+function saludar(nombre='visitante'){
+    //if(typeof nombre === 'undefined'){nombre='Pablo'} froma vieja
+
+    return `hola ${nombre}`;
+}
+saludo = saludar();
+console.log(saludo);
+/* function expression */
+const suma4= function(a=3,b=4){
+    return a+b;
+} 
+
+console.log(suma4(1,2));
+console.log(suma4(5));
+/* otro tipo de funciones IIFE */
+/*  se llaman solas  */
+(function(tecnologia){
+    console.log(`creando un IIFE en ${tecnologia}`);
+})('javascript');
+
+/* metodos de propiedad, cuando una funcion se pone destro de este objeto */
+
+const musica={
+    reproducir: function(id){
+        console.log(`reproduciendo cancion id ${id}`);
+    },
+    pausar:function(){
+        console.log(`Pause a la musica`);
+    }
+
+}
+
+/* los metodos tambien se pueden agregar afuera del objeto */
+musica.borrar=function(id){
+    console.log(`Borrando la cancion con id: ${id}`);
+}
+
+musica.reproducir(30);
+musica.pausar();
+musica.borrar(2);
