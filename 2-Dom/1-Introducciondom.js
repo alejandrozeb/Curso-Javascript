67594ce07bfbab9e8ae4078ebe2da157e47ccc06
unknown <alejandrojzeballos@gmail.com>
2020-06-15 22:06:37 -0400

    Dom en js

---
+++ 2-Dom/1-Introducciondom.js
@@ -0,0 +1,13 @@
+/* DOM es Document Object Model */
+
+/* cuando deseas leer las etiquetas puedes obtener el objeto document con todas las etiquetas e ID's */
+
+/* el padre el es documetno
+    root puedes encontrar dos partes
+        head el cual tiene elementos
+            title y por ultimo el texto
+        body tabien tiene elementos
+            nav ,a, tambien textos y clases
+            
+        en h1 - texto - id - class podemos modificar todos estos elementos*/
+/* se puede seleccionar e ir de nodo en nodo */
