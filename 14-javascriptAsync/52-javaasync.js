61d2d80cebd6c5b17bb1f02bed75ef5c15287ee0
unknown <alejandrojzeballos@gmail.com>
2020-07-05 01:13:07 -0400

    javasincrono parte 2

--- 14-javascriptAsync/52-javaasync.js
+++ 14-javascriptAsync/52-javaasync.js
@@ -18,9 +18,16 @@ ciudades.forEach(callback);
 /* si cargas desde una rest api */
 
 const paises=['Francia','España', 'Portugal', 'Australia', 'Inglaterra','Irlanda'];
+//se ejecutara despues
+function nuevoPais(pais,callback){
+    setTimeout(function(){
+        paises.push(pais);
+        callback();//es como si llamaramos a la funcion mostrar paises
+    },2000);
+}
 
 //simularemos que traemos de una api con settime o una bd
-
+//los paises se muestran despues de un segundo
 function mostrarPaises(){
     setTimeout(function(){
         let html= '';
@@ -30,5 +37,13 @@ function mostrarPaises(){
         document.getElementById('app').innerHTML=html;
     },1000);
 }
+
+//agregar nuevo pais
+nuevoPais('Alemania',mostrarPaises);
+
 //Mostrar los paises
 mostrarPaises();
+
+
+/* no se muestra alemania por que la peticion nuevo pais se ejecuta despues en tiempo de mostrar paises */
+/* funciones en cadena */
