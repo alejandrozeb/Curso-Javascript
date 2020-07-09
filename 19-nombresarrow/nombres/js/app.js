2d077b1db53d149a56caf244c336e8446a9edb2f
unknown <alejandrojzeballos@gmail.com>
2020-07-08 23:53:54 -0400

    proyecto nombres con arrow en js

---
+++ 19-nombresarrow/nombres/js/app.js
@@ -0,0 +1,54 @@
+
+document.querySelector('#generar-nombre').addEventListener('submit', cargarNombres);
+
+// Llamado a Ajax e imprimir resultados
+function cargarNombres(e) {
+     e.preventDefault();
+
+     // Leer las variables
+
+     const origen = document.getElementById('origen');
+     const origenSeleccionado = origen.options[origen.selectedIndex].value;
+
+     const genero = document.getElementById('genero');
+     const generoSeleccionado = genero.options[genero.selectedIndex].value;
+
+     const cantidad = document.getElementById('numero').value;
+
+    
+
+     let url = '';
+     url += 'http://uinames.com/api/?';
+     // Si hay origen agregarlo a la URL
+     if(origenSeleccionado !== '') {
+          url += `region=${origenSeleccionado}&`;
+     }
+     // Si hay un genero agregarlo a la URL
+     if(generoSeleccionado !== '') {
+          url += `gender=${generoSeleccionado}&`;
+     }
+     // Si hay una cantidad agregarlo a la URL
+     if(cantidad !== '') {
+          url += `amount=${cantidad}&`;
+     }
+
+     // Código de FETCH API AQUI
+
+     //crear fecth
+
+     fetch(url)
+          .then(res => res.json())
+          .then(data =>{
+               let html = '<h2> Nombres Generados >/h2>';
+               html += `<ul class="lista"`;
+               data.forEach(nombre => {              
+                    html += `
+                    <li>${nombre.name}</li>
+                    `;
+               }); 
+
+               html += '</ul>'
+               document.querySelector('#resultado').innerHTML = html;
+          })
+          .catch(error =>console.log(error));
+}
