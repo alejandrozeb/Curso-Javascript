1e50add2159b7d8202533bb8af9d696ebb0c5379
unknown <alejandrojzeballos@gmail.com>
2020-07-13 23:57:30 -0400

    leyendo datos del formulario

--- 22-proyecto8eventos/eventos/js/app.js
+++ 22-proyecto8eventos/eventos/js/app.js
@@ -2,3 +2,30 @@
 
 const eventbrite = new EventBrite();
 const ui = new Interfaz();
+
+//lsitenner al bucador
+
+document.getElementById('buscarBtn').addEventListener('click', (e) =>{
+    e.preventDefault();
+   // console.log('presionando');
+
+    //leer el texto del input buscar
+
+    const textoBuscador = document.getElementById('evento').value;
+
+    //leer el select
+
+    const categorias = document.getElementById('listado-categorias');//selecciono el listado
+    const categoriaeleccionada = categorias.options[categorias.selectedIndex].value;//saco del select el value
+
+    console.log(textoBuscador);
+    console.log(categoriaeleccionada);
+
+    //revisar que haya algo escrito en el buscador
+
+    if(textoBuscador != ''){//verifico si esta vacio
+        console.log(buscnado);
+    }else{
+        console.log('no hay nada');
+    }
+} )
