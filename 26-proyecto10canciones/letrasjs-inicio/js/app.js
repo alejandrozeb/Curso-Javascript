e7eeab356c9d90a1e9aa854b1667a92d3b1d020a
unknown <alejandrojzeballos@gmail.com>
2020-07-20 23:36:19 -0400

    iniciando el proyecto creando el modulo de interfaz

---
+++ 26-proyecto10canciones/letrasjs-inicio/js/app.js
@@ -0,0 +1,4 @@
+/* aqui va toda la funcionalidad */
+
+import * as UI from './interfaz.js';
+console.log(UI);
---
+++ 26-proyecto10canciones/letrasjs-inicio/js/interfaz.js
@@ -0,0 +1,5 @@
+//seleccionamos el formulario
+export const formularioBuscar = document.querySelector('#formulario-buscar'),
+divBuscar =  document.querySelector('#buscar'),
+divMensaje =  document.querySelector('#mensajes'),
+divResultado =  document.querySelector('#resultado');
