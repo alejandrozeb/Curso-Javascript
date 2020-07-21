9c7ba52fc8a83247053a948104c0cb1d0a5ae3df
unknown <alejandrojzeballos@gmail.com>
2020-07-21 02:14:04 -0400

    Introduccion a patrones de diseño

---
+++ 27-patronesdediseño/68-patronesdediseño.js
@@ -0,0 +1,34 @@
+/* Patrones de Diseño 
+
+que son?
+Son soluciones de codigo re-utilizables que ese utilizan en problemas comunes en aplicaciones de software(o javaScript)
+
+Cada problema de programcion es diferente por lo tanto el patron puede ser similar pero el codigo siempre es diferente
+
+Por lo normal en una empresa se emplean los patrones de diseño para que todo el codigo siga el mismo lineamineto, y toos lo puedan entender y mantener
+
+---Categorias de Patronrs----
+de creacion de objetos: cada lenguaje tiene su forma de crear objetos, pero es mediante un patron de diseño de este tipo que se pueden crear objetos que sigan el mismo lineamiento.
+ej: Factory, Abstrarct Factory, builder, prototype, singleton
+
+de estructura: definen la interdependencia y comunicacion entre objetos, en algunas ocasiones si un objeto cambia, como afecta a otros objetos que dependen de el.
+ej: adapter, bridge, composite, decorator, facade, flyweight, proxy
+
+de comportamiento: defiinen la interdependencia y comunicacion entre objetos, en algunas ocacsiones si un objeto cambia, como afecta a otros objetos que depende de el. 
+ej Tempalte Method, observer, mediator, state, visitor
+
+de arquitectura:(carpetas nombres y como se relacionan) definen funcionalidad especifica sobre la arquitectura de una aplicacion como carpetas, nombres, que hace cada archivo y como se relacionan. 
+
+Usualmente los 3 anteriores son utilizados en uno de arqutectura
+
+ej MVC(model view controller)
+    MVP(model view presenter)
+    MVVM(model view view model)
+    frameworks como maria, spineJS, Ember, backOne o angular siguen estas arquitecturas.
+
+los patrones de diseño estan presentes en toddos lso lenguajes de programacion.
+preguntas de trabajo
+que es un factory 
+como creas un builder
+
+*/
