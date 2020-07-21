959b4aefc4d7104f82080c947ea96808cf72b5af
unknown <alejandrojzeballos@gmail.com>
2020-07-21 00:31:44 -0400

    validando el formulario con espacios vacios

--- 26-proyecto10canciones/letrasjs-inicio/js/app.js
+++ 26-proyecto10canciones/letrasjs-inicio/js/app.js
@@ -2,3 +2,34 @@
 
 import * as UI from './interfaz.js';
 console.log(UI);
+
+//escuchamos el submit del formulario
+
+UI.formularioBuscar.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    //aqui usaremos las variables, por que el exportar se realiza cuando carga la pagina
+    
+    //obtener los datos del formulario
+
+    const artista = document.querySelector('#artista').value,
+          cancion = document.querySelector('#cancion').value;
+
+          console.log(artista);
+          console.log(cancion);
+
+          if(artista === '' || cancion === ''){
+              //el usuario dja los capos vacios mostrar error
+                UI.divMensaje.innerHTML='Error... todos los campos son obligatorios';
+                UI.divMensaje.classList.add('error');
+                //para limpiar
+                setTimeout(() => {
+                    UI.divMensaje.innerHTML='';
+                    UI.divMensaje.classList.remove('error');    
+                }, 3000);
+          }else{
+            //el formulario esta completo, realizar consulta a la API
+          }
+
+
+});
--- 26-proyecto10canciones/letrasjs-inicio/js/interfaz.js
+++ 26-proyecto10canciones/letrasjs-inicio/js/interfaz.js
@@ -3,3 +3,5 @@ export const formularioBuscar = document.querySelector('#formulario-buscar'),
 divBuscar =  document.querySelector('#buscar'),
 divMensaje =  document.querySelector('#mensajes'),
 divResultado =  document.querySelector('#resultado');
+
+//se recomienda psar los datos estaticos del ui, los dinamicos los usamos en app
