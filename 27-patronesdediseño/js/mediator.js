1037d9e6aa705fcdb321823a6c3ba51f53540792
unknown <alejandrojzeballos@gmail.com>
2020-07-23 02:17:54 -0400

    mediator, agregando la clase subasta que sera el mediator

--- 27-patronesdediseño/js/mediator.js
+++ 27-patronesdediseño/js/mediator.js
@@ -4,11 +4,14 @@
 
 const Vendedor = function(nombre){
     this.nombre = nombre;
+    this.sala = null;
 }
 
 const Comprador = function(nombre){
     this.nombre = nombre;
+    this.sala = null;
 }
+
 
 Vendedor.prototype = {
     oferta: function(articulo, precio){
@@ -20,8 +23,52 @@ Vendedor.prototype = {
     }
 }
 
-comprador.prototype = {
+Comprador.prototype = {
     oferta: function(mensaje, comprador){
         console.log(`${comprador.nombre}: ${mensaje}`);
     }
 }
+//creamos la clase que sera el mediador
+const Subasta = function(){
+    let compradores = {};
+
+    return{
+        registrar: function(usuario){
+            compradores[usuario.nombre]= usuario;   //pasamos que usuario se esta registrando
+            usuario.sala = this;    //crearemos una sala para la subastea
+            console.log(compradores);
+
+        }
+    }
+}
+
+//instanciar las clases
+
+const juan = new Comprador('Juan');
+const pablo = new Comprador('Pablo');
+const karen = new Comprador('Karen');
+
+const vendedor= new Vendedor('Vendedor');   //se instancia pero las salas no estan aun definidas
+
+const subasta = new Subasta();
+subasta.registrar(juan);
+subasta.registrar(karen);
+subasta.registrar(pablo);
+
+//console.log(juan);
+console.log(subasta);
+
+
+const subasta2 = new Subasta();
+subasta2.registrar(juan);
+
+//console.log(juan);
+console.log(subasta2);
+/* Los chats tambien son ejemplos de mediator */
+
+vendedor.oferta('Mustang 1960', 3000);
+juan.oferta(3500, juan);
+pablo.oferta(4000, pablo);
+karen.oferta(10000, karen);
+
+vendedor.vendido(karen.nombre);
