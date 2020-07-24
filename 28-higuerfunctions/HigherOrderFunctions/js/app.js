f643ad40a33ae6dd70681d157bed670be8d367a5
unknown <alejandrojzeballos@gmail.com>
2020-07-23 23:34:53 -0400

    HiguerAarrayfunctions mostrando como funciona el for

---
+++ 28-higuerfunctions/HigherOrderFunctions/js/app.js
@@ -0,0 +1,163 @@
+/* es un array con objetos */
+const autos = [
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2012,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A4', year: 2018, precio: 40000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2015,
+		precio: 20000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A6', year: 2010, precio: 35000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2016,
+		precio: 70000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2015,
+		precio: 25000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Chevrolet',
+		modelo: 'Camaro',
+		year: 2018,
+		precio: 60000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{ marca: 'Ford', modelo: 'Mustang', year: 2019, precio: 80000, puertas: 2, color: 'Rojo', transmision: 'manual' },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2017,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A3', year: 2017, precio: 55000, puertas: 2, color: 'Negro', transmision: 'manual' },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2012,
+		precio: 25000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 45000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2019,
+		precio: 90000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Ford', modelo: 'Mustang', year: 2017, precio: 60000, puertas: 2, color: 'Negro', transmision: 'manual' },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2015,
+		precio: 35000,
+		puertas: 2,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2018,
+		precio: 50000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2017,
+		precio: 80000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A4', year: 2016, precio: 30000, puertas: 4, color: 'Azul', transmision: 'automatico' }
+];
+
+//console.table(autos);//para mostrar como tabla
+
+//si queremos listar todos los modelos anteriormente se hacia con un for 
+
+for(let i=0; i < autos.length; i++){
+	console.log(autos[i].marca);
+}
+console.log('-------------------000-----');
+//lisatdo mayor a 40000
+for(let i=0; i < autos.length; i++){
+	if(autos[i].precio > 40000){
+		console.log(autos[i].marca);
+	}
+}
+//listado de puertas
+for(let i=0; i < autos.length; i++){
+	if(autos[i].puertas === 4){
+		console.log(autos[i]);
+	}
+}
+
+//solo teniamos este tipo para recorrer un objeto, array u otras estructuras
+
+// forEach
+
+// map
+
+// filter
+
+// find
+
+// reduce
+
+// some
+
+
