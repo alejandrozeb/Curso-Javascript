50b6b62f3433e9ad3a2e7e7232c9fb4765d01cb1
unknown <alejandrojzeballos@gmail.com>
2020-07-23 23:21:12 -0400

    Namespace parte ejecutando las funciones

--- 27-patronesdediseño/js/Namespace.js
+++ 27-patronesdediseño/js/Namespace.js
@@ -2,7 +2,7 @@
 //namespace
 const restaurApp = {};
 //los platillos
-restaurApp.platillos = [
+restaurApp.platillos = [//es un arreeglo de objetos
     {
         platillo: 'Pizza',
         precio: 25,
@@ -19,16 +19,45 @@ restaurApp.platillos = [
 //funciones, con space
 
 restaurApp.funciones = {
+    //status del platillo o pedir platillo
     ordenar: id => {
         console.log(` tu platillo: ${restaurApp.platillos[id].platillo} se esta preparando`);
+    },
+    //crea un platillo nuevo
+    agregarPlatillo: (platillo, precio) =>{
+        const nuevo={
+            platillo,
+            precio
+        }
+        restaurApp.platillos.push(nuevo);
+    },
+    //muetsra el menu
+    mostrarMenu: platillos => {
+        console.log(`Bienvenido a nuetsro menu`);
+        platillos.forEach((platillo, index) => {
+            console.log(`${index} : ${platillo.platillo} ${platillo.precio}`);            
+        });
     }
 }
 
 
 console.log(restaurApp);
-restaurApp.funciones.ordenar(2);
-
-/* se usa librerias y algunas ya tiene nuestras variables por eso se recomienda usar nemaespace */
 
 
-console.log(restaurApp);
+//una de las ventjas de namespace es que se puede aplicar destructuring
+
+const {platillos} = restaurApp; //sacamos con destrcuturing del array retaurApp los platillos.
+
+restaurApp.funciones.mostrarMenu(platillos);
+//a partir de ver el menu podemos ordenar
+restaurApp.funciones.ordenar(0);
+
+//agregando platillo
+
+restaurApp.funciones.agregarPlatillo('Pastel', 15);
+restaurApp.funciones.mostrarMenu(platillos);
+
+
+/* se usa librerias y algunas ya tiene nuestras variables por eso se recomienda usar nemaespace, es un manejo en estructura  */
+/* jquery usa este tipo de patron de diseño */
+
