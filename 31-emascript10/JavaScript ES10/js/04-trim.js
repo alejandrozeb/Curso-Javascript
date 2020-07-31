83042dccdecb5ced13fe916326a01448256ab343
unknown <alejandrojzeballos@gmail.com>
2020-07-30 23:42:13 -0400

    trim, eliminando espacios en js

--- 31-emascript10/JavaScript ES10/js/04-trim.js
+++ 31-emascript10/JavaScript ES10/js/04-trim.js
@@ -1,1 +1,14 @@
+//trim
 const correo = '    correo@correo.com    ';
+
+//trim start permite eliminar el espacio en blanco de una cadena de texto
+
+console.log(correo);        //imprime con espacios
+
+console.log(correo.trimStart());    //elimna los espacios del inicio
+
+console.log(correo.trimEnd());  //eliimina los espacios del final
+
+console.log(correo.trimStart().trimEnd());  //elimina del inicio y el final
+
+console.log(correo.trim()); //elimina espacios, desde siempre
