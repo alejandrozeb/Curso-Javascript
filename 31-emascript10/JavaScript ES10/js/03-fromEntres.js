1fd8f020aa69df5b9a88ab5ee847ee2a2056cc4d
unknown <alejandrojzeballos@gmail.com>
2020-07-30 22:29:26 -0400

    fromEntries, transformando a objetos

--- 31-emascript10/JavaScript ES10/js/03-fromEntres.js
+++ 31-emascript10/JavaScript ES10/js/03-fromEntres.js
@@ -1,4 +1,22 @@
+/* este es un map */
+
 const map = new Map([ 
     ['nombre', 'Producto 1'], 
     ['precio', 20] 
 ]);
+
+//fromentres, te permite crear un objeto de una serie de llaves y valores
+console.log(map);
+console.log('-------------objeto----------');
+/* cambia a un objeto */
+const objeto = Object.fromEntries(map);
+console.log(objeto);
+
+delete objeto.precio;  //usando metoos de objetos
+console.log(objeto);
+
+console.log(map);
+map.delete('precio');
+console.log(map);
+
+// se puede aplicar en un set y en un map, siempre y cuando tenga llave y valor
