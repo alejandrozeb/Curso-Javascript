55a42ae376be1c434b643bfd2410f203f67af681
unknown <alejandrojzeballos@gmail.com>
2020-08-04 15:56:16 -0400

    creando un script con npm y usando el script del package.json con webpack

--- 33-webpack/webpack.config.js
+++ 33-webpack/webpack.config.js
@@ -9,5 +9,9 @@ module.exports = {
         path: path.join(__dirname, '/dist')//crea el driectorio
     }
     //node_modules\.bin\webpack corremos este comando y vemos que creo el dist
+    //para acortar este paso odemos ir al package y en scripts crear que se va a ejecutar con el siguiente comnado.
+    //npm run ejecutar
+    //el ejecutar es el mismo que especificamos en los scripts ademas que decimos el modo (production or development)
+    //para realizar una prueba puedes borrar la carpeta dist y ejecutar el comando anterior.
 }
 
