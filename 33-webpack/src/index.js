4dc163bf7d2e5f8e6d7c442a3222a47abf225014
unknown <alejandrojzeballos@gmail.com>
2020-08-04 00:18:16 -0400

    creando nuestro primer bundle

---
+++ 33-webpack/src/index.js
@@ -0,0 +1,6 @@
+/* creando nuestro primer Bundle */
+/* en dist/main.js webpack crea codigo para manejar estos modulos en el que va incluido este */
+const carrito = ['Producto 1', 'Producto 2', 'Producto 3'];
+console.log(carrito);
+
+/* el archivo main.js es nuestro primer bundle y sabemos que trabaja por que mostramos este array desde ese archivo */
