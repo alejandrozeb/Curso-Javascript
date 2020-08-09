286f35d9c21915efb839e9f98bb48decfba8f223
unknown <alejandrojzeballos@gmail.com>
2020-08-08 23:49:18 -0400

    segunda forma de crear componetnes

--- 35-react/proyecto/prestamos/src/componentes/Header.js
+++ 35-react/proyecto/prestamos/src/componentes/Header.js
@@ -1,17 +1,11 @@
 import React, {Fragment} from 'react';  //importamos react
 
-//un componente siempre sera una funcion.
-//primer componente
-function Header({titulo}){
-     //aqui podemos colocar codigo estandar de js
-     
-     
 
-    return(                 //es una funcion de react y lo que devolvemos es la vista
+//function declaretion or function expression con sfc y enter se crea
+const Header = ({titulo}) =>( 
         <Fragment>
             <h1>{titulo}</h1>
         </Fragment>
-    )
-}
+     );
 
 export default Header;
