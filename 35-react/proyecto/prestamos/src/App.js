b8ad0cd22e6c4959163238a3984f67027fa3179b
unknown <alejandrojzeballos@gmail.com>
2020-08-08 22:29:05 -0400

    Realizando nuestro hola mundo con componentes y usando fragment

--- 35-react/proyecto/prestamos/src/App.js
+++ 35-react/proyecto/prestamos/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-
+import React, {Fragment} from 'react';
+import Header from './componentes/Header';
 function App() {
   return (
-    <div className="App">
-     <h1>Hola Mundo</h1>
-    </div>
+    <Fragment>
+        <Header />  {/* comentario asi llamamos un componente */}
+        <Header />
+        <Header />
+        <Header />
+        <Header />
+    </Fragment>
   );
 }
 
---
+++ 35-react/proyecto/prestamos/src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';  //importamos react
+
+//un componente siempre sera una funcion.
+//primer componente
+function Header(){
+    
+    return(                 //es una funcion de react y lo que devolvemos es la vista
+        <h1>Hola Mundo</h1>
+    )
+}
+
+export default Header;
