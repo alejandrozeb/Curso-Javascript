8e2667d2649eb0e0f690a8b316cc415d830f559b
unknown <alejandrojzeballos@gmail.com>
2020-08-14 00:29:54 -0400

    consultando a la base de datos y editando la ruta para enviar nustra consulta

--- 36-nodejs/proyecto/IntronodeJS/server/routes/index.js
+++ 36-nodejs/proyecto/IntronodeJS/server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//importamos el modelo
+const Viaje = require('../models/Viajes');
 module.exports = function(){
     router.get('/',(req,res) => {
         res.render('index');
@@ -13,11 +15,13 @@ module.exports = function(){
     });
 
     router.get('/viajes',(req,res) => {
-        res.render('viajes', {
-            pagina: 'Proximos viajes'
-        });
+        Viaje.findAll()
+            .then(viajes => res.render('viajes', {
+                pagina: 'Proximos viajes',
+                viajes
+            }))
+            .catch(error => console.log(error))
     });
     //controlador?
-
     return router;
 }
