7fca3f0bc76f48d7878615797f26e5346b2e219f
unknown <alejandrojzeballos@gmail.com>
2020-08-15 00:33:13 -0400

    creando la tabla y el modelo para testimoniales

---
+++ 36-nodejs/proyecto/IntronodeJS/server/models/Testimoniales.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Testimonial = db.define('testimoniales',{
+    nombre : {
+        type: Sequelize.STRING
+    },
+    correo : {
+        type: Sequelize.STRING
+    },
+    mensaje : {
+        type: Sequelize.STRING
+    }
+});
+module.exports = Testimonial;
