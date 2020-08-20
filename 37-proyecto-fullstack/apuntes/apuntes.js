31444b4af2d998f1323d13102e27a9e736845e05
unknown <alejandrojzeballos@gmail.com>
2020-08-19 20:14:37 -0400

    agregando el proyecto fullstack iniciando con la api y sus dependencia de produccion y desarrollo

---
+++ 37-proyecto-fullstack/apuntes/apuntes.js
@@ -0,0 +1,18 @@
+/* Construiremos una app con una api donde se haran CRUD con express como servidor y el frontend sera con react, veremos la construccion de esta y la integracion, ademas crearemos una aplicacion de escritorio que trabajar con la misma app  */
+
+//  API
+
+/* 
+creamos la carpeta api con el archivo index.js.
+damos el inicio con 
+npm init 
+en la consola 
+llenamos los campos
+y ahora instalaremos dependencias.
+npm install express@4.17.1 mongoose@7.7.7
+
+mongoose nos permite administrar usar la base de datos
+ahora instalaremos dependencias que solo usaremos durante el desarrollo
+nodemon cada vez que se realize un cambio los detectara y va a reinicar el servidor con esos cambios.
+
+*/
