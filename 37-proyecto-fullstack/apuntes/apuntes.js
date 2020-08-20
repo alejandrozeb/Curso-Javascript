64a8f8417088f8567299a63df3235897fbe3b88c
unknown <alejandrojzeballos@gmail.com>
2020-08-20 02:13:17 -0400

    agregando el modelo paciente

--- 37-proyecto-fullstack/apuntes/apuntes.js
+++ 37-proyecto-fullstack/apuntes/apuntes.js
@@ -56,5 +56,41 @@ mongoose.connect('mongodb://localhost/veterinaria',{
     useUnifiedTopology: true,
     useFindAndModify: false
 })
+//git 
+usar cada vez que termine la vista.
+definiendo el modelo
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //crea la estructura en la base de datos
+
+
+//definimos una nueva tabla (Schema)
+
+const pacientesSchema = new Schema({
+    nombre: {
+        type: String,               //tipos, pueden ser arrays
+        trim: true,                 //quita los espacios extra de la cadena
+    },
+    propietario:{
+        type: String,
+        trim: true
+    },
+    fecha:{
+        type: String,      //con el date agraga la hora en la que se a registrado pero no necesitamos sa informacion
+        trim: true
+    },
+    hora:{
+        type: String,
+        trim: true
+    },
+    sintomas:{
+        type: String,
+        trim: true
+    }
+});
+
+module.exports = mongoose.model('Paciente', pacientesSchema);
+//moongose todo lo tiene a una sola instancias y podemos hacer referencia a paciente
+
+creando el controlador
 
 */
