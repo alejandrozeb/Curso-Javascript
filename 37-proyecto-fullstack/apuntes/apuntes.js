28f930a65db582c1991b53ab2aef89fa2bebb65a
unknown <alejandrojzeballos@gmail.com>
2020-08-26 00:20:57 -0400

    agregando la primera pagina con electron

--- 37-proyecto-fullstack/apuntes/apuntes.js
+++ 37-proyecto-fullstack/apuntes/apuntes.js
@@ -467,5 +467,32 @@ npm i -D electron@7.1.0     //la que usaremos.
 
 luego cambiamos el script en package.json
 "start": "electron -"
+Configuracion de electron.
+
+const {app, BrowserWindow} = require('electron');
+
+
+let appWindow;      //debe estar por fuera, por que electrons e encarga de la memoria y cundado recarga y esta adentro se lo lleva
+function crearVentana(){
+    //aqui pasamostoda la configuracion necesaria
+    appWindow = new BrowserWindow({
+
+    });
+    //cuando la aplicacion es cerrada
+    appWindow.on('closed', () => {
+        appWindow = null;
+    });
+    //cargar html
+    appWindow.loadFile('./index.html'); //pasamos la ruta.
+    //cuando la app este lista, mostrar la ventana
+    appWindow.once('ready-to-show', () =>{
+        appWindow.show();   //muestra
+    })
+}
+
+app.on('ready', crearVentana);//es un evento cuando todo este listo le pasamos nuestra ventana.
+
+electron lee el html incluido con bootstrap o algun archivo css
+
 
             */
